b601c1dc744ff77950a829bbcaeed2ef6926ab5f
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-02-28 11:15:03 +0100

    add route files

---
+++ src/routes/resortRoutes.js
@@ -0,0 +1,9 @@
+//api/v1/resorts
+const express = require('express')
+const router = express.Router()
+
+//get all resorts
+//get resort by id
+//get resorts from city id, per 10
+
+module.exports = router
