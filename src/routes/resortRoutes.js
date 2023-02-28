cf77c84fffe344c743d80482b5d725256bda1b2b
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-02-28 12:02:00 +0100

    add error handling

--- src/routes/resortRoutes.js
+++ src/routes/resortRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 //get all resorts
 //get resort by id
 //get resorts from city id, per 10
+//get resort by id, per 10, sort by rating
 
 module.exports = router
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,4 @@
 //api/v1/users
+
+//put om auth om admin -> resort
+//put om auth not admin -> user
