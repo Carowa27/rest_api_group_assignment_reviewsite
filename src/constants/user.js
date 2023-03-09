52829c04c4b62586623ab51a77cb52a0e858c1be
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-03-09 17:07:27 +0100

    upd userConst.

--- src/constants/user.js
+++ src/constants/user.js
@@ -2,9 +2,3 @@ exports.userRoles = {
   USER: "USER",
   ADMIN: "ADMIN",
 };
-
-//ändra namn på denna då vi inte har listor, accountRoles?
-// exports.listRoles = {
-//   owner: "owner",
-//   guest: "guest",
-// };
